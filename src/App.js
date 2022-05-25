e5a6f32215f4b3ffa0ea1323eb73a4aa06aa8eab
unknown <sabinorotondi2000@gmail.com>
2022-05-25 23:54:21 +0200

    feat: renamed exs

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-//import Hello from './Esercizi svolti/01'
-//import Name from './Esercizi svolti/02'
+//import Hello from './Esercizi svolti/JSX - 01'
+//import Name from './Esercizi svolti/JSX - 02'
 import Sum from './Esercizi svolti/03'
 import HelloSciuti from './Esercizi svolti/05'
 import AppSciuti from './Esercizi svolti/06'
--- src/Esercizi svolti/Components - 03.js
+++ src/Esercizi svolti/Components - 03.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import HelloSciuti from "./06"
+import HelloSciuti from "./Components - 03"
 
 class AppSciuti extends Component {
   render() {
