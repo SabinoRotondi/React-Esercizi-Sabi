24297d90634f210e1202edd82c9d96bbd7c709fc
unknown <sabinorotondi2000@gmail.com>
2022-05-31 13:41:33 +0200

    clean

--- src/Esercizi svolti/JSX01.js
+++
@@ -1,7 +0,0 @@
-const Hello = () => {
-    return (
-        <h1>Hello, world!</h1>
-    )
-}
-
-export default Hello
--- src/Esercizi svolti/JSX05.js
+++
@@ -1,8 +0,0 @@
-function Sum () { 
-    const sum = (a, b) => a + b
-    return (
-        <h2>La somma è {sum(1, 2)}</h2>
-    )
-}
-
-export default Sum
