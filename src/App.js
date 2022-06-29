881699b7346c3a6faf161d17a6ba17e58e77dc4d
unknown <sabinorotondi2000@gmail.com>
2022-06-29 15:48:41 +0200

    feat: created branch and  blank files

--- src/App.js
+++ src/App.js
@@ -1,16 +1,7 @@
-// import ClickCounter from "./Esercizi svolti/useEffect01";
-// import ClickCounterSec from "./Esercizi svolti/useEffect02";
-// import GithubUser from "./Esercizi svolti/useEffect03";
-import GithubUsersList from "./Esercizi svolti/useEffect04";
-
 function App() {
-  // const onCounterChange = (counter) => console.log(`the counter is ${counter}`)
   return (
     <div className="App">
-      {/* <ClickCounter change={onCounterChange} initialValue={2} /> */}
-      {/* <ClickCounterSec /> */}
-      {/* <GithubUser username='SabinoRotondi'/> */}
-      <GithubUsersList />
+
     </div>
   );
 }
