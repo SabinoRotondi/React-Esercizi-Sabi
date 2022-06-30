c364b11ba41740b0e66b6774fa805dd0c4f93f28
unknown <sabinorotondi2000@gmail.com>
2022-06-30 16:52:29 +0200

    feat:SWR blanks

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-import React from "react";
-import { Counter } from "./Esercizi svolti/Counter";
 
 function App() {
   return (
       <div className="App">
-        <Counter />
+
       </div>
   );
 }
